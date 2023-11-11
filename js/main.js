a837a0f41767ec39a0e50695a46db616d11c87b9
Jakub Patera <patera.jakub686@gmail.com>
2023-11-11 12:17:38 +0100

    edit menu paddin to hide H1

--- js/main.js
+++ js/main.js
@@ -1,69 +1,69 @@
-/* SHOW MENU*/ 
+/* SHOW MENU*/
 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show-menu')
-        })
-    }
-}
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
 
-showMenu('nav-toggle', 'nav-menu')
+showMenu("nav-toggle", "nav-menu");
 
 /* Remove Mobile Menu */
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /* Scroll to active link */
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+  sections.forEach((curent) => {
+    const sectionHeight = curent.offsetHesight;
+    const sectionTop = curent.offsetTop - 50;
+    sectionId = curent.getAttribute("id");
 
-    sections.forEach(curent => {
-        const sectionHeight = curent.offsetHesight
-        const sectionTop = curent.offsetTop - 50;
-        sectionId = curent.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.remove('active-link')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
 
-window.addEventListener('scroll', scrollActive)
-
+window.addEventListener("scroll", scrollActive);
 
 /* Header BG change */
 
-function scrollHeader(){
-    const nav = document.getElementById('header')
+function scrollHeader() {
+  const nav = document.getElementById("header");
 
-    if(this.scrollY >= 200) nav.classList.add('scroll-header');
-    else nav.classList.remove('scroll-header')
+  if (this.scrollY >= 2000) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
-window.addEventListener('scroll',scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
+/* -- Scroll Top -- */
 
-/* -- Scroll Top -- */ 
+function scrollTop() {
+  const nav = document.getElementById("scroll-top");
 
-
-function scrollTop(){
-    const nav = document.getElementById('scroll-top')
-
-    if(this.scrollY >= 650) nav.classList.add('scroll-top');
-    else nav.classList.remove('scroll-top')
+  if (this.scrollY >= 6500) nav.classList.add("scroll-top");
+  else nav.classList.remove("scroll-top");
 }
-window.addEventListener('scroll',scrollTop)
+window.addEventListener("scroll", scrollTop);
